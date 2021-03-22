89dff490d3087754191921a5e07cb1219d4f6b4d
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-22 01:17:34 -0700

    Added json file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
 import About from "./pages/about";
 import EmployeeDirectory from "./pages/EmployeeDirectory";
 import Navbar from "./components/navbar";
