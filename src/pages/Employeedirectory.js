f8a22b51ffdb075b4e94bc250f30b6d0028f23b8
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 22:26:41 -0700

    state search

--- src/pages/Employeedirectory.js
+++ src/pages/Employeedirectory.js
@@ -1,5 +1,20 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import Container from "../components/container";
-import PageHeader from "../components/pageheader";
-import EmployeesTable from "../components/Employeetable";
+import PageHeader from "../components/pageHeader";
+import EmployeesTable from "../components/EmployeeTable";
+
+class EmployeeDirectory extends Component {
+    state = {
+        search: "",
+        employees: [],
+        nameSort: "ascending",
+        genderSort: "ascending",
+        ageSort: "ascending",
+        phoneSort: "ascending",
+        emailSort: "ascending",
+        addressSort: "ascending",
+        matchedEmployees: [],
+      }; 
+}
+
