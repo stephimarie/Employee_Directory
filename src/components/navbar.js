0a5c5cb61c0b9c58b2827ed8d7ab830d99bb0fcd
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 20:22:49 -0700

    link navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,3 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "";
 
+function Navbar() {
+    return (
+        <nav className="topNav">
+            <a>
+                <NavLink to="/" activeClassName="active" exact={true}>
+                    About
+                </NavLink>
+            </a>
+            <a>
+            </a>
+        </nav>
+    )
+}
