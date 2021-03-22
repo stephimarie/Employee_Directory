3eee61f65b77802a5a94ce57fc8de616986ebc0f
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 19:41:15 -0700

    first init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Nav from './Nav';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1>Hello World</h1>
+
+      <div>
+        <Nav />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+
+
+function Nav() {
+  return (
+    <div>
+
+      <div>
+        <h3> Goodbye all</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
