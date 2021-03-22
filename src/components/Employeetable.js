153a0fd252b4e6d4ec4765abf3ae55879f7ea2b9
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 21:10:35 -0700

    fix spacing

--- src/components/Employeetable.js
+++ src/components/Employeetable.js
@@ -8,59 +8,62 @@ function EmployeeTable(props) {
             <Row>
                 <Column size="sm-7"></Column>
                 <Column size="sm-5">
-                    <div className="input-icons">
-                        <IconContext.Provider
-                            value={{
-                                color: "grey",
-                                className: "icon",
-                            }}
-                        >
-                            <i>
-                                <FiSearch />
-                            </i>
-                        </IconContext.Provider>
-                        <input 
-                        value={props.search}
-                        className="input-field"
-                        type="text"
-                        onChange={props.filterTable}
-                        placeholder="Search.."
-                        ></input>
-                    </div>
-                    <IconContext.Provider
-                        value={{
-                            color: "grey",
-                            className: "button-icon",
-                        }}
-                    >
-                        <button
-                            className="icon-button"
-                            onClick={props.refreshTableFunction}
-                        >
-                            <FiRefreshCcw />
-                            Refresh
-                        </button>
-                    </IconContext.Provider>
-                    <div className="dropdown">
+                    <div class="button-area">
+                        <div className="input-icons">
+                            <IconContext.Provider
+                                value={{
+                                    color: "grey",
+                                    className: "icon",
+                                }}
+                            >
+                                <i>
+                                    <FiSearch />
+                                </i>
+                            </IconContext.Provider>
+                            <input 
+                                value={props.search}
+                                className="input-field"
+                                type="text"
+                                onChange={props.filterTable}
+                                placeholder="Search.."
+                            ></input>
+                        </div>
                         <IconContext.Provider
                             value={{
                                 color: "grey",
                                 className: "button-icon",
                             }}
                         >
-                            <button className="dropdown-button">
-                                <FiFilter />
-                                Filter
+                            <button
+                                className="icon-button"
+                                onClick={props.refreshTableFunction}
+                            >
+                                <FiRefreshCcw />
+                                Refresh
                             </button>
                         </IconContext.Provider>
-                        <div class="dropdown-content">
-                            <li onClick={props.genderFilterMale}>Male</li>
-                            <li onClick={props.genderFilterFemale}>Female</li>
+                        <div className="dropdown">
+                            <IconContext.Provider
+                                value={{
+                                    color: "grey",
+                                    className: "button-icon",
+                                }}
+                            >
+                                <button className="dropdown-button">
+                                    <FiFilter />
+                                    Filter
+                                </button>
+                                </IconContext.Provider>
+                                <div class="dropdown-content">
+                                    <li onClick={props.genderFilterMale}>Male</li>
+                                    <li onClick={props.genderFilterFemale}>Female</li>
+                                </div>
                         </div>
-
                     </div>
                 </Column>
             </Row>
+
+            
         </div>
     );
 }
