0a3caf277ab641f49f123976b586f1980169ba5e
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 20:11:03 -0700

    Added footer

---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "";
+
+function Footer() {
+    return (
+        <footer className="footer p-2">
+      <span>© Copyright 2021 Stephanie Marie</span>
+        </footer>
+    );
+}
+
+export default Footer;
