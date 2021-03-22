04b68c8ac89d397df041afa2391c753fff383df2
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-21 22:31:26 -0700

    get request setup

--- src/pages/Employeedirectory.js
+++ src/pages/Employeedirectory.js
@@ -20,6 +20,34 @@ class EmployeeDirectory extends Component {
   componentDidMount() {
     this.getEmployees();
   }
+  getEmployees = () => {
+    API.getEmployees()
+      .then((res) => {
+        const results = res.data.results;
+        const employeeArray = [];
+        for (let i = 0; i < results.length; i++) {
+          const row = {
+            picture: results[i].picture.medium,
+            name: `${results[i].name.first} ${results[i].name.last}`,
+            gender: results[i].gender,
+            age: results[i].dob.age,
+            phone: results[i].phone,
+            email: results[i].email,
+            address: `${results[i].location.street.number} ${results[i].location.street.name}. ${results[i].location.city}, ${results[i].location.state} ${results[i].location.postcode}`,
+          };
+          employeeArray.push(row);
+        }
+        console.log(employeeArray);
+
+        this.setState({
+          employees: employeeArray,
+          search: "",
+          matchedEmployees: [],
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
 
 }
 
