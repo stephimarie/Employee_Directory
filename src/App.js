0c988e2eba91fd686ab298258305a527b3acad75
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-22 00:21:29 -0700

    fix link appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import About from "./pages/About";
+import About from "./pages/about";
 import EmployeeDirectory from "./pages/EmployeeDirectory";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import Wrapper from "./components/wrapper";
+import Footer from "./components/footer";
 
 function App() {
   return (
